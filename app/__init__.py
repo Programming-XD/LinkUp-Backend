f5959c54eda2a31ce47bd21fd84d06be9875f2e5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-08 15:50:18 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,20 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
 import logging
+from variables import *
 
+# LOGGING
+logging.basicConfig(
+    format="[LinkUp-Backend] %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+
+# VARIABLES
+MONGO_DB_URL = os.environ.get("MONGO_DB_URL") or VAR_MONGO_DB_URL
+
+
+# DATABASE
+logging.info('Starting database...')
+DATABASE = AsyncIOMotorClient(MONGO_DB_URI)["LinkUp"]
