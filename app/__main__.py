6b5313aeae9e1a6ef776bc6a9d1ccfdcbe1e71d7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-09 14:42:03 +0530

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -6,13 +6,12 @@ from app.functions.message import message_bp
 import asyncio
 
 # REGISTER
-
 app.register_blueprint(login_bp)
 app.register_blueprint(message_bp)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'server online'
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
