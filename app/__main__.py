6d596f3889011cfad343041d394d2fe01d42d7ec
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-09 12:42:47 +0530

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from app import app
 from app.functions.login import login_bp
+import asyncio
 
 # REGISTER
 
@@ -11,6 +12,11 @@ app.register_blueprint(login_bp)
 def home():
     return 'Hello, World!'
 
-if __name__ == "__main__":
+def run():
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port) 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(app.run(debug=True, host="0.0.0.0", port=port))
+
+if __name__ == "__main__":
+    run()
+    
