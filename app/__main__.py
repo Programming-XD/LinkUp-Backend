17c19d384ca45f96db35ef79453b75d20d19e394
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-09 11:31:38 +0530

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from app import app
 
-app = Flask(__name__)
+@app.route('/', methods=['POST'])
+async def ntg():
+    return 'hi'
 
 if __name__ == '__main__':
     app.run(debug=True)
