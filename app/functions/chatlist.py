ef1a1340e5777aa1b4db6bd7badabcb8c87b5551
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-25 23:51:31 +0530

    Create chatlist.py

---
+++ app/functions/chatlist.py
@@ -0,0 +1,22 @@
+from app import app
+from app.database.message import Message
+from app.database.user import User
+from quart import request, jsonify, Blueprint
+
+message_bp = Blueprint('chatlist', __name__)
+user = User()
+
+@app.route('/chatlist/', methods=['POST'])
+async def chatlist():
+    data = await request.get_json()
+    session = data.get('session')
+    if not session:
+        return jsonify({"error": "Where is session?"})
+    user_info = await user.get_user_details(username)
+    if user_info['session'] != session:
+        return jsonify({"error": "Invalid session"})
+    elif not user_info:
+        return jsonify({"error": "Invalid user"})
+    chats = user_info["chatlist"] or []
+    return jsonify({"chats": chats})
+    
