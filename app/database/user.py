4f3cc8f4cc6699b1463b78a1abe5b77cab6975a2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-26 11:30:47 +0530

    Update user.py

--- app/database/user.py
+++ app/database/user.py
@@ -13,9 +13,9 @@ class User:
             list_users = list_users.get("users", [])
             return list_users
 
-    async def get_user_details(self, username, check_available=False):
+    async def get_user_details(self, user_id, check_available=False):
         if check_available:
-            user = await db.find_one({"_id": username})
+            user = await db.find_one({"_id": user_id})
             if not user:
                 return False
             return True
@@ -25,11 +25,11 @@ class User:
                 return False
             return user
 
-    async def session(self, username, password, create_or_delete='create'):
+    async def session(self, user_id, password, create_or_delete='create'):
         if create_or_delete == 'create':
-            if await self.get_user_details(username, True):
+            if await self.get_user_details(user_id, True):
                 session_string = secrets.token_hex(30)
-                session_string = f"{username}@{session_string}"
+                session_string = f"{user_id}@{session_string}"
                 return session_string
             else:
                 return 'INVALID USER'
@@ -37,6 +37,7 @@ class User:
     async def sign_up(self, name, username, password):
         list_users = await self.get_users()
         username = username.lower()
+        
         if username in list_users:
             return 'User exists'
         if len(password) > 14:
@@ -51,41 +52,52 @@ class User:
             return 'Name too big'
         if len(name) <= 3:
             return 'Name too small'
-        await db.insert_one({"_id": username, "name": name, "profile_picture": "https://i.imgur.com/juKF4kK.jpeg", "password": password, "session": None, "chats": []})
-        await db.update_one({"_id": 1}, {"$addToSet": {"users": username}}, upsert=True)
-        session_string = await self.session(username, password)
-        await db.update_one({"_id": username}, {"$set": {"session": session_string}}) 
+            
+        
+        latest_user = await db.find_one({"_id": 1})
+        if not latest_user:
+            latest_user = 142
+        else:
+            latest_user = latest_user.get("latest_user") or 142
+            latest_user += 1
+        await db.update_one({"_id": 1}, {"$set": {"latest_user": latest_user}})
+        await db.update_one({"_id": 1}, {"$addToSet": {"users": latest_user}, upsert=True)
+        await db.insert_one({"_id": latest_user, "name": name, "profile_picture": "https://i.imgur.com/juKF4kK.jpeg", "password": password, "session": None, "username": username, "chats": []})
+        
+                                         
+        session_string = await self.session(latest_user, password)
+        await db.update_one({"_id": latest_user}, {"$set": {"session": session_string}}) 
         return f"success: {session_string}"
 
-    async def login(self, username=None, password=None, session=None):
-        username = username.lower()
+    async def login(self, user_id=None, password=None, session=None):
+        user_id = user_id.lower()
         if session:
-            get_user_details = await self.get_user_details(username)
+            get_user_details = await self.get_user_details(user_id)
             if '@' not in session:
                 return 'INVALID SESSION FORMAT'
-            username = session.split('@')[0]
+            user_id = session.split('@')[0]
             session_string = get_user_details['session']
             if session == session_string:
                 return f"success: {session_string}"
             else:
                 return 'INVALID SESSION'
         else:
-            if not await self.get_user_details(username, True):
+            if not await self.get_user_details(user_id, True):
                 return 'INVALID USER'
             else:
-                get_user_details = await self.get_user_details(username)
+                get_user_details = await self.get_user_details(user_id)
                 original_password = get_user_details['password']
                 if original_password == password:
-                    session_string = await self.session(username, password)
-                    await db.update_one({"_id": username}, {"$set": {"session": session_string}})
+                    session_string = await self.session(user_id, password)
+                    await db.update_one({"_id": user_id}, {"$set": {"session": session_string}})
                     return f"success: {session_string}"
                 else:
                     return 'WRONG PASSWORD'
 
-    async def add_chat(self, username, chat_data, chat_username):
-        await db.update_one({"_id": username}, {"$push": {"chats": chat_data}}, upsert=True)
-        chats = await self.get_user_details(username)
+    async def add_chat(self, user_id, chat_data, chat_id):
+        await db.update_one({"_id": user_id}, {"$push": {"chats": chat_data}}, upsert=True)
+        chats = await self.get_user_details(user_id)
         chats = chats.get('chatlist') or []
-        if chat_username not in chats:
-            await db.update_one({"_id": username}, {"$addToSet": {"chatlist": chat_username}}, upsert=True)
+        if chat_id not in chats:
+            await db.update_one({"_id": user_id}, {"$addToSet": {"chatlist": chat_id}}, upsert=True)
         
