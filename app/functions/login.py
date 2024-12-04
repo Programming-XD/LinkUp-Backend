3b71978e104125de882e011cb92ad58a13478521
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-04 15:48:50 +0530

    Update login.py

--- app/functions/login.py
+++ app/functions/login.py
@@ -6,23 +6,6 @@ user = User()
 
 login_bp = Blueprint('login', __name__)
 
-@app.route('/signup/', methods=['POST'])
-async def sign_up():
-    data = await request.get_json()
-    name = data.get('name')
-    username = data.get('username')
-    password = data.get('password')
-
-    if not name or not username or not password:
-        return jsonify({"error": "All fields are required!"}), 400
-
-    result = await user.sign_up(name, username, password)
-
-    if result.startswith('success'):
-        return jsonify({"message": result}), 200
-    else:
-        return jsonify({"error": result}), 400
-
 
 @app.route('/login/', methods=['POST'])
 async def login():
