fec6e6c7bad0990fb74b2264aefbafbcedc93751
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-04 17:09:33 +0530

    Update chatlist.py

--- app/functions/chatlist.py
+++ app/functions/chatlist.py
@@ -11,11 +11,11 @@ async def chatlist():
     data = await request.get_json()
     session = data.get('session')
     if not session:
-        return jsonify({"error": "Where is session?"})
+        return jsonify({"error": "Where is session?"}), 400
     get_chats = await user.get_chats(session)
     if get_chats == 'INVALID SESSION':
-        return jsonify({"error": "INVALID SESSION"})
+        return jsonify({"error": "INVALID SESSION"}), 400
     elif get_chats == "INVALID USER":
-        return jsonify({"error": "INVALID SESSION"})
+        return jsonify({"error": "INVALID USER"}), 400
     return jsonify({"chats": get_chats}), 200
     
