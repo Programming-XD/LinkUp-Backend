f52e92cce5ba44f3a14014e249ec22fd2b865a3e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-04 15:49:42 +0530

    Create signup.py

---
+++ app/functions/signup.py
@@ -0,0 +1,25 @@
+from app import app
+from app.database.user import User
+from quart import request, jsonify, Blueprint
+
+user = User()
+
+signup_bp = Blueprint('signup', __name__)
+
+@app.route('/signup/', methods=['POST'])
+async def sign_up():
+    data = await request.get_json()
+    name = data.get('name')
+    username = data.get('username')
+    password = data.get('password')
+
+    if not name or not username or not password:
+        return jsonify({"error": "All fields are required!"}), 400
+
+    result = await user.sign_up(name, username, password)
+
+    if result.startswith('success'):
+        return jsonify({"message": result}), 200
+    else:
+        return jsonify({"error": result}), 400
+
