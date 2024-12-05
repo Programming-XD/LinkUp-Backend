4e3c2575f5e64f57734c27dafdb7cca7ad4f04e7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-05 18:14:48 +0530

    Update user_info.py

--- app/functions/user_info.py
+++ app/functions/user_info.py
@@ -11,16 +11,17 @@ async def userinfo():
   data = await request.get_json()
   session = data.get('session') or None
   chat_id = str(data.get('chat_id')) or None
-  user_id = chat_id # Ded with 1000 errors lol
+  user_id = session.split('@')[0] # Ded with 1000 errors lol
   if not session:
     return jsonify({"error": "Where is session"}), 400
   elif not chat_id:
     return jsonify({"error": "Hmm, ig you finding your gf id isn't?"}), 400
   
-  if chat_id.isdigit():
+  if chat_id.isdigit() and user_id.isdigit():
     chat_id = int(chat_id)
+    user_id = int(user_id)
   else:
-    return jsonify({"error": "Invalid chat_id"}), 400
+    return jsonify({"error": "Invalid chat_id/session"}), 400
   session_stats = await user.session(user_id=user_id, create_or_delete="chk", session=session)
   if session_stats == "Same":
     details = await user.get_user_details(chat_id)
