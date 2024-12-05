6e9e7d6c5a33544019927380e4dc68b4b35fe03e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-05 15:19:50 +0530

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -7,6 +7,7 @@ from app.functions.logs import logs_bp
 from app.functions.chatlist import chatlist_bp
 from app.functions.signup import signup_bp
 from app.functions.check_session import check_session_bp
+from app.functions.user_info import user_info_bp
 from quart_cors import cors
 import asyncio
 from quart import request, jsonify, Blueprint
@@ -21,7 +22,7 @@ app.register_blueprint(logs_bp)
 app.register_blueprint(chatlist_bp)
 app.register_blueprint(check_session_bp)
 app.register_blueprint(signup_bp)
-
+app.register_blueprint(user_info_bp)
 
     
 @app.route('/')
