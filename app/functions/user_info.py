35ab2061b1e5bfcffcf381b3edef9c51ef4259d1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-05 15:15:28 +0530

    Create user_info.py

---
+++ app/functions/user_info.py
@@ -0,0 +1,26 @@
+from app import app
+from app.database.message import Message
+from app.database.user import User
+from quart import request, jsonify, Blueprint
+
+userinfo_bp = Blueprint('userinfo', __name__)
+user = User()
+
+@app.route('/userinfo/', methods=['POST'])
+async def userinfo():
+  data = await request.get_json()
+  session = data.get('session') or None
+  if not session:
+    return jsonify({"error": "Where is session"}), 400
+  user_id = session.split('@')[0]
+  if user_id.isdigit():
+    user_id = int(user_id)
+  else:
+    return jsonify({"error": "Invalid session"}), 400
+  session_stats = await user.session(user_id=user_id, create_or_delete="chk", session=session)
+  if session_stats == "Same":
+    h
+  elif session_stats == "WRONG":
+    return jsonify({"error": "Invalid session"}), 400
+  else:
+    return jsonify({"error": "Invalid session/User"}), 400
