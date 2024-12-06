490f6722c650ddc840adba8975ce43223bc39d7e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-06 23:21:59 +0530

    Update user_info.py

--- app/functions/user_info.py
+++ app/functions/user_info.py
@@ -12,11 +12,13 @@ async def userinfo():
   data = await request.get_json()
   session = data.get('session') or None
   chat_id = str(data.get('chat_id')) or None
-  user_id = session.split('@')[0] # Ded with 1000 errors lol
+  user_id = str(session.split('@')[0]) 
   if not session:
     return jsonify({"error": "Where is session"}), 400
   elif not chat_id:
     return jsonify({"error": "Hmm, ig you finding your gf id isn't?"}), 400
+  elif chat_id == user_id:
+    return jsonify({'error': "Why user_id and chat_id is same???"}), 400
   
   if chat_id.isdigit() and user_id.isdigit():
     chat_id = int(chat_id)
@@ -27,16 +29,19 @@ async def userinfo():
   if session_stats == "Same":
     try:
       details = await user.get_user_details(chat_id)
-      output = {
-        'profile_picture': details.get('profile_picture'),
-        'name': details.get('name'),
-        'username': details.get('username')
-      }
-      return jsonify({"output": output}), 200
+      if details:
+        output = {
+          'profile_picture': details.get('profile_picture'),
+          'name': details.get('name'),
+          'username': details.get('username')
+        }
+        return jsonify({"output": output}), 200
+      else:
+        return jsonify({"error": f"Invalid chat id"}), 400
     except Exception as e:
       logging.error(f'Error while fetching, details of user: {user_id}, Error: {e}')
       return jsonify({"error": f'Error while fetching, details of user: {user_id}, Error: {e}'}), 400
   elif session_stats == "WRONG":
-    return jsonify({"error": f"{session_stats} session"}), 400
+    return jsonify({"error": f"Wrong session"}), 400
   else:
     return jsonify({"error": f"Invalid session/User & {session_stats}"}), 400
