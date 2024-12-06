eb1ecd7a80753b905e841a63104e48264da7dfbf
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-06 10:34:58 +0530

    Create test.py

---
+++ app/ws/test.py
@@ -0,0 +1,12 @@
+from app import *
+from quart import *
+
+test_bp = Blueprint('test', __name__)
+
+@app.websocket('/ws/test/')
+async def test():
+  while True:
+    msg = await websocket.receive()
+    if msg:
+      await websocket.send(msg)
+    await websocket.send('Idk she love me or not')
