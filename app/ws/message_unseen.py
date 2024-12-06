00f56b19a91f12878ac044385d56b0237f02ca77
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-06 13:07:42 +0530

    Update message_unseen.py

--- app/ws/message_unseen.py
+++ app/ws/message_unseen.py
@@ -12,38 +12,44 @@ message = Message()
 
 @message_unseen_bp.websocket('/ws/msguns/')
 async def message_unseen():
-  logging.info('someone coming: %s', await websocket.receive())
-  data = json.loads(await websocket.receive())
-  session = data.get('session')
-  if not session:
-    await websocket.send(json.dumps({'error': 'Session required'}))
-    logging.info("Sent error")
+  try:
+    data = json.loads(await websocket.receive())
+    logging.info('someone coming: %s', data)
+    await websocket.send('I received your message')
+    session = data.get('session')
+    user_id = str(session.split('@')[0])
+    if not session:
+      await websocket.send(json.dumps({'error': 'Session required'}))
+      logging.info("Sent error")
+      await websocket.close()
+      return 
+    elif not user_id.isdigit():
+      await websocket.send(json.dumps({'error': 'Invalid session'}))
+      logging.info("Sent error")
+      await websocket.close()
+      return 
+    user_id = int(user_id)
+    user_details = await user.get_user_details(user_id)
+    if not user_details or user_details.get('session') != session:
+      await websocket.send(json.dumps({"error": "Invalid session or user"}))
+      logging.info("Sent error")
+      await websocket.close()
+      return 
+    old_msg = {}
+    await websocket.send("Start listening for new messages!")
+    logging.info("Sent")
+    while True:
+      messages = await message.receive_new_messages(user_id=user_id, session=session)
+      if old_msg != messages:
+        if isinstance(messages, str):
+          await websocket.send(json.dumps({"error": messages}))
+        else:
+          await websocket.send(json.dumps({"messages": messages}))
+        logging.info("Sent")
+      await asyncio.sleep(0.3)
+  except Exception as e:
+    logging.error(str(e))
+    await websocket.send(str(e))
     await websocket.close()
-    return 
-  user_id = str(session.split('@')[0])
-  if not user_id.isdigit():
-    await websocket.send(json.dumps({'error': 'Invalid session'}))
-    logging.info("Sent error")
-    await websocket.close()
-    return 
-  user_id = int(user_id)
-  user_details = await user.get_user_details(user_id)
-  if not user_details or user_details.get('session') != session:
-    await websocket.send(json.dumps({"error": "Invalid session or user"}))
-    logging.info("Sent error")
-    await websocket.close()
-    return 
-  old_msg = {}
-  await websocket.send("Start listening for new messages!")
-  logging.info("Sent")
-  while True:
-    messages = await message.receive_new_messages(user_id=user_id, session=session)
-    if old_msg != messages:
-      if isinstance(messages, str):
-        await websocket.send(json.dumps({"error": messages}))
-      else:
-        await websocket.send(json.dumps({"messages": messages}))
-      logging.info("Sent")
-    await asyncio.sleep(0.3)
     
     
