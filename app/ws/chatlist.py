1e98acb6113465ca9935b2305ee27640eadc16bf
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-06 17:42:10 +0530

    Create chatlist.py

---
+++ app/ws/chatlist.py
@@ -0,0 +1,38 @@
+from app import *
+from quart import *
+from app.database.message import Message
+from app.database.user import User
+import json
+import asyncio
+import logging
+
+chatlist_bp = Blueprint('chatlist', __name__)
+user = User()
+
+@chatlist_bp.websocket('/ws/chatlist/')
+async def chatlist():
+    try:
+        w = websocket 
+        data = json.loads(await websocket.receive())
+        session = data.get('session')
+        if not session:
+            await w.send(json.dumps({"error": "Where is session?"}))
+            return await w.close(code=1002)
+        old_chatlist = []
+        while True:
+            get_chats = await user.get_chats(session)
+            if get_chats == 'INVALID SESSION':
+                await w.send(json.dumps({"error": "INVALID SESSION"}))
+                return await w.close(code=1002)
+            elif get_chats == "INVALID USER":
+                await w.send(json.dumps({"error": "INVALID USER"}))
+                return await w.close(code=1002)
+            if get_chats != old_chatlist:
+                await w.send(json.dumps({"chats": get_chats}))
+                old_chatlist = get_chats
+            await asyncio.sleep(0.3)
+    except Exception as e:
+        logging.error(str(e))
+        await websocket.send(str(e))
+        await websocket.close(code=1002)
+
