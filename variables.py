194775cb8d8e49a00881a4b6794789830bab5b77
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-10 14:23:53 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,1 @@
-VAR_MONGO_DB_URL = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
+VAR_MONGO_DB_URL = "mongodb+srv://otazuki004:mongodbpas143@A@cluster0.c8mbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
